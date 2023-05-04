121a860745f72437c6799f93d47c0746839e1fd4
ghobadkhan <arian.ghb@gmail.com>
2023-05-04 17:21:20 -0400

    WIP: commenting, documenting and pipelining

--- problem_1.py
+++ problem_1.py
@@ -12,15 +12,31 @@ reg = re.compile(r".*?(\w*#?)\.csv")
 
 
 def download_and_unpack():
+    """
+    I use python-dotenv package to register and import kaggle creds from a .env file.
+    The sample is included.
+    """
+
     from kaggle.api.kaggle_api_extended import KaggleApi
     api = KaggleApi()
     api.authenticate()
-    # api.dataset_download_cli(dataset="jacksoncrow/stock-market-dataset")
     api.dataset_download_files(dataset="jacksoncrow/stock-market-dataset", path="data/initial")
     with zipfile.ZipFile("stock-market-dataset.zip", 'r') as zip_ref:
         zip_ref.extractall("data/initial/stock_market_dataset")
 
 def write_securities():
+    """
+    One efficient way to read a large amount of data from disk is using dask dataframe.
+    The logic is as follows:
+    1- read the symbols metadata using normal pandas dataframe
+    2- per each security type (etfs, stocks) run the following logic:
+        a - read the related dataset into dask dataframe
+        b - repartition the dataframe to more efficiently use the memory and io operations
+        c - floor and change the type of 'Volume' column
+        d - convert the added 'path' column to 'Symbol' using regex (taking only the file name in the path)
+        e - merge the symbol metadata dataframe with the main one to add the corp name corresponding to each symbol
+        f - save the results
+    """
     symbols_df = pd.read_csv("data/initial/stock_market_dataset/symbols_valid_meta.csv",usecols=["Symbol","Security Name"])
     for sec_type, n_partitions in [("stocks",100),("etfs",30)]:
         df = dd.read_csv(f"data/initial/stock_market_dataset/{sec_type}/*.csv",include_path_column=True,assume_missing=True)
@@ -32,9 +48,9 @@ def write_securities():
         df.to_parquet(f"data/stage_1/{sec_type}",write_index=False)
 
 def run():
-    # os.makedirs("data/initial", exist_ok=True)
-    # download_and_unpack()
-    return write_securities()
+    os.makedirs("data/initial", exist_ok=True)
+    download_and_unpack()
+    write_securities()
     
 
 if __name__ == "__main__":
--- problem_2.py
+++ problem_2.py
@@ -1,17 +1,26 @@
 import dask.dataframe as dd
 
 def calculate_rolling(dask_df):
+    # From the loaded dask dataframe (passed as param), get the full data of these 3 columns: "Symbol","Volume","Adj Close"
+    # Since the info is already in daily freq and the order is preserved, no further sorting and filtering is needed
     samp = dask_df[["Symbol","Volume","Adj Close"]].compute()
-    mov_avg_volume = samp[["Symbol","Volume"]].groupby("Symbol").rolling(30).mean().rename(columns={"Volume":"vol_moving_avg"})
-    mov_median_adj_close = samp[["Symbol","Adj Close"]].groupby("Symbol").rolling(30).median().rename(columns={"Adj Close":"adj_close_rolling_med"})
+    
+    # Calculation of rolling values:
+    # Note: If we calculate the rolling values without grouping, the initial 29 days values of each symbol is wrongly 
+    # calculated from the last symbol.
+    mov_avg_volume = samp[["Symbol","Volume"]].groupby("Symbol").rolling(30).mean()\
+        .rename(columns={"Volume":"vol_moving_avg"})
+    mov_median_adj_close = samp[["Symbol","Adj Close"]].groupby("Symbol").rolling(30).median()\
+        .rename(columns={"Adj Close":"adj_close_rolling_med"})
+    # Joining the newly calculated value to the present dataframe
     return dask_df.join(mov_avg_volume.join(mov_median_adj_close).reset_index(drop=True))
 
 def run():
     for sec_type in ["stocks","etfs"]:
+        # Read the saved parquet data into dask dataframe
         df = dd.read_parquet(f"data/stage_1/{sec_type}")
+        # Calculation of rolling values and joining to the current dataframe
         df = calculate_rolling(df)
+        # Saving the updated dataframe
         df.to_parquet(f"data/stage_2/{sec_type}",write_index=False)
-
-if __name__ == "__main__":
-    run()
     
